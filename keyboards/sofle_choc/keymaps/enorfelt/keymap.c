c3c4a5486d77b79f218e858a3afa5a6df46bc4d5
Erik Norfelt <enorfelt@outlook.com>
2024-02-27 07:14:35 +0100

    layers

--- keyboards/sofle_choc/keymaps/enorfelt/keymap.c
+++ keyboards/sofle_choc/keymaps/enorfelt/keymap.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-//#include "keymap_swedish.h"
+#include "keymap_swedish.h"
 
 #define _QWERTY 0
 #define _SYMBOLS 1
@@ -44,39 +44,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   KC_MPLY,KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
                       KC_LGUI, KC_LCTL, KC_LALT, MO(_SYMBOLS), KC_ENT,    KC_SPC, MO(_NAVIGATION),  KC_BSPC,  KC_DEL, KC_RALT
-
 ),
 /*
  * SYMBOLS
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  §   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  +   |
+ * |  §   |   !  |   @  |   #  |   $  |   %  |                    |   &  |   /  |   `  |   ´  |   ^  |  ¨   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TAB  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   7  |   8  |   9  |   P  |  Å   |
+ * | TAB  |      |   *  |   ~  |   {  |   }  |                    |      |   7  |   8  |   9  |   :  |  "   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   4  |   5  |   6  |   Ö  |  Ä   |
+ * | ESC  |   =  |   +  |   -  |   (  |   )  |-------.    ,-------|   ?  |   4  |   5  |   6  |   ;  |  '   |
  * |------+------+------+------+------+------|  Mute |    | Pause |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   0  |   1  |   2  |   3  |   -  | RCTL |
+ * |LShift|   |  |   <  |   >  |   [  |   ]  |-------|    |-------|   0  |   1  |   2  |   3  |   \  | RCTL |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LCTL | LALT | SYMB | /Enter  /       \Space \  | NAVI | BSPC |  DEL | RALT |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
-
 [_SYMBOLS] = LAYOUT(
-    KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,                       KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,                       KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,                       KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_TRNS,   KC_TRNS,KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+    KC_TRNS,  SE_EXLM,SE_AT,   SE_HASH, SE_DLR,  SE_PERC,                     SE_AMPR,SE_SLSH, SE_GRV,  SE_ACUT, SE_CIRC,   SE_DIAE,
+    KC_TRNS,  KC_NO,  SE_ASTR, SE_TILD, SE_LCBR, SE_RCBR,                     KC_NO,  KC_P7,   KC_P8,   KC_P9,   SE_COLN,   SE_DQUO,
+    KC_TRNS,  SE_EQL, SE_PLUS, SE_MINS, SE_LPRN, SE_RPRN,                     SE_QUES,KC_P4,   KC_P5,   KC_P6,   SE_SCLN,   SE_QUOT,
+    KC_TRNS,  SE_PIPE,SE_LABK, SE_RABK, SE_LBRC, SE_RBRC, KC_TRNS,    KC_TRNS,KC_P0,  KC_P1,   KC_P2,   KC_P3,   SE_BSLS,   KC_TRNS,
                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,MO(_ADJUST),  KC_TRNS,  KC_TRNS, KC_TRNS
-
 ),
+/*
+ * SYMBOLS
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  §   |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | TAB  |      |      |      |      |      |                    |NUMLK | HOME |  UP  | END  |      | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | ESC  |      |      |      |      |      |-------.    ,-------| CAPS | LEFT | DOWN |RIGHT |      |      |
+ * |------+------+------+------+------+------|  Mute |    | Pause |------+------+------+------+------+------|
+ * |LShift| UNDO |  CUT | COPY |PASTE |      |-------|    |-------|PRTSC | PGUP |      | PGDN |      | RCTL |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LCTL | LALT | SYMB | /Enter  /       \Space \  | NAVI | BSPC |  DEL | RALT |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
 [_NAVIGATION] = LAYOUT(
     KC_TRNS,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,    KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,
     KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_NUM,   KC_HOME,  KC_UP,   KC_END,  KC_NO,   KC_F12,
     KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_CAPS,  KC_LEFT,  KC_DOWN, KC_RGHT, KC_NO,   KC_NO,
-    KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_TRNS,  KC_TRNS,KC_PSCR,  KC_PGUP,  KC_NO,   KC_PGDN, KC_NO,   KC_TRNS,
+    KC_TRNS,  KC_UNDO,KC_CUT,  KC_COPY, KC_PASTE,KC_NO,    KC_TRNS,  KC_TRNS,KC_PSCR,  KC_PGUP,  KC_NO,   KC_PGDN, KC_NO,   KC_TRNS,
                       KC_TRNS, KC_TRNS, KC_TRNS, MO(_ADJUST), KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
-
 ),
 [_ADJUST] = LAYOUT(
     RGB_TOG,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
@@ -84,7 +95,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_RMOD, RGB_HUD,RGB_SAD, RGB_VAD, RGB_SPD, KC_NO,                      KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_TRNS,  KC_TRNS,KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
                       KC_TRNS, KC_TRNS, KC_TRNS, MO(_ADJUST), KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
-
 ),
 };
 
